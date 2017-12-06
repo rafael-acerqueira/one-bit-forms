fccdbce623ce90e280408039193a03493c1260b3
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-12-06 13:50:42 -0300

    Ajust parameter in Answer method to accept params from client

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,16 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :form
   has_many :questions_answers, dependent: :destroy
-  # accepts_nested_attributes_for :questions_answers
-  validates :form, presence: true
+  accepts_nested_attributes_for :questions_answers
 
-
-  def self.create_with_questions_answers form, questions_answers
+  def self.create_with_question_answers form, questions_answers
     answer = nil
     ActiveRecord::Base.transaction do
       answer = Answer.create(form: form)
       questions_answers.each do |qa|
-        answer.questions_answers.create(value: qa['value'], question_id: qa['question']['id'])
+        answer.questions_answers.create(qa.permit(:question_id, :value))
       end
     end
     answer
